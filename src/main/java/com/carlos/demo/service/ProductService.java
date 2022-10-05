80b703792d9575e719294081cfeb07e01c332010
Carvelog <carvelog@gmail.com>
2022-10-05 16:05:24 +0100

    minor change

--- src/main/java/com/carlos/demo/service/ProductService.java
+++ src/main/java/com/carlos/demo/service/ProductService.java
@@ -5,7 +5,6 @@ import com.carlos.demo.repository.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
